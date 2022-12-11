6c625a6e109b8be24c4a7692abb9b78e328fe2c8
jangseoyoung98 <77472570+jangseoyoung98@users.noreply.github.com>
2022-12-11 17:04:36 +0900

    BirdJump #1
    - make and use the instance of Rigidbody2D
    - use Vector2

--- FlappyClone/Assets/BirdJump.cs
+++ FlappyClone/Assets/BirdJump.cs
@@ -5,18 +5,26 @@ using UnityEngine;
 
 public class BirdJump : MonoBehaviour // 상속(MonoBehaviour)
 {
+    Rigidbody2D rb; // 자료형 (속성으로 사용했던 거 ->  클래스)
+
+
+
     // Start is called before the first frame update
     void Start()
     {
         // 코드 실행 시, 처음 한 번만 실행
-        Debug.Log("Start입니다~!"); // print
+        rb = GetComponent<Rigidbody2D>(); // 컴포넌트 가져옴
+
     }
 
     // Update is called once per frame
     void Update()
     {
         // 매 '프레임'마다 실행 (60fps -> 1초에 60 프레임)
-        Debug.Log("Update입니다~!"); // print
+        if (Input.GetMouseButtonDown(0)) // 마우스 왼쪽 버튼 눌렀을 때 -> 점프
+        {
+            rb.velocity = Vector2.up * 3;// (0,1) * 3 -> 위로 3만큼 점프함
+        }
 
     }
 }
